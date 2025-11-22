001a7c520b0a75fa93a0a9c6e2603d19bbec1369
ekram78278 <ekram78278@gmail.com>
2025-11-22 18:33:11 +0600

    Fix all image paths for production deployment

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,16 +1,16 @@
-
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-
 // Import Swiper styles
-
 import 'swiper/css/navigation';
 import 'swiper/css';
 
-
-
-
+// Import images
+import heroImage1 from '../assets/hero-image.jpg';
+import heroImage2 from '../assets/hero-image2.jpg';
+import heroImage3 from '../assets/hero-image3.jpg';
+import heroImage4 from '../assets/hero-image4.jpg';
+import heroImage5 from '../assets/hero-image5.jpg';
 
 // import required modules
 import { Autoplay, Navigation } from 'swiper/modules';
@@ -32,35 +32,35 @@ const Slider = () => {
         <SwiperSlide>
           <img
             className="w-full h-[300px] sm:h-[400px] md:h-[500px] object-cover"
-            src="/src/assets/hero-image.jpg"  // ✅ Fixed: Put in /public folder
+            src={heroImage1}
             alt="Hero 1"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             className="w-full h-[300px] sm:h-[400px] md:h-[500px] object-cover"
-            src="/src/assets/hero-image2.jpg"
+            src={heroImage2}
             alt="Hero 2"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             className="w-full h-[300px] sm:h-[400px] md:h-[500px] object-cover"
-            src="/src/assets/hero-image3.jpg"
+            src={heroImage3}
             alt="Hero 3"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             className="w-full h-[300px] sm:h-[400px] md:h-[500px] object-cover"
-            src="/src/assets/hero-image4.jpg"
+            src={heroImage4}
             alt="Hero 4"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
             className="w-full h-[300px] sm:h-[400px] md:h-[500px] object-cover"
-            src="/src/assets/hero-image5.jpg"
+            src={heroImage5}
             alt="Hero 5"
           />
         </SwiperSlide>
